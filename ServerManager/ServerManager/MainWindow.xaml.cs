d1355fe3e0b1d16b77625cc2bc7ba89a1bbd7e59
Karan Bhamra <karan.bhamra@gmail.com>
2017-03-12 22:21:56 -0700

    Version 1.0 released
    Added ability to save log, and handles found errors more gracefully.

--- ServerManager/ServerManager/MainWindow.xaml.cs
+++ ServerManager/ServerManager/MainWindow.xaml.cs
@@ -19,100 +19,135 @@ using System.Windows.Threading;
 
 namespace ServerManager
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        private SshClient sshclient;
-        private string address;
-        private string user;
-        private string pass;
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private SshClient sshclient;
+		private string address;
+		private string user;
+		private string pass;
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            disconnectButton.IsEnabled = false;
-            command.IsEnabled = false;
-        }
+		public MainWindow()
+		{
+			InitializeComponent();
+			disconnectButton.IsEnabled = false;
+			command.IsEnabled = false;
+			commandButton.IsEnabled = false;
+			logButton.IsEnabled = false;
+		}
 
-        private void Connect_Button(object sender, RoutedEventArgs e)
-        {
-            var connection = Connect();
-            sshclient = new SshClient(connection);
-            sshclient.Connect();
-            output.Text = $"Connected successfully to {host.Text} as {username.Text}\n";
-            var uptime = sshclient.CreateCommand("uptime");
-            uptime.Execute();
-            output.Text += $"\nUptime {uptime.Result}";
-            connectButton.IsEnabled = false;
-            disconnectButton.IsEnabled = true;
-            command.IsEnabled = true;
+		private void Connect_Button(object sender, RoutedEventArgs e)
+		{
+			var connection = Connect();
+			if (connection != null)
+			{
+				sshclient = new SshClient(connection);
+				try
+				{
+					sshclient.Connect();
+					output.Text = $"Connected successfully to {host.Text} as {username.Text}\n";
+					var uptime = sshclient.CreateCommand("uptime");
+					uptime.Execute();
+					output.Text += $"\nUptime {uptime.Result}";
+					connectButton.IsEnabled = false;
+					disconnectButton.IsEnabled = true;
+					command.IsEnabled = true;
+					commandButton.IsEnabled = true;
+					logButton.IsEnabled = true;
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show("Failed to connect.\nCheck server details.");
+					Console.WriteLine(ex.Message);
+
+				}
+			}
+
+		}
+
+		public ConnectionInfo Connect()
+		{
+			address = host.Text;
+			user = username.Text;
+			pass = password.Password;
+			ConnectionInfo connection = null;
+			try
+			{
+				connection = new ConnectionInfo(address, user,
+				  new AuthenticationMethod[]
+				  {
+					new PasswordAuthenticationMethod(user,pass)
+				  });
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Failed to connect.\nCheck server details.");
+				Console.WriteLine(ex.Message);
+			}
+			return connection;
+		}
+
+		private void disconnectButton_Click(object sender, RoutedEventArgs e)
+		{
+			closeConnection();
+			output.Text += "\nDisconnected from server.";
+			connectButton.IsEnabled = true;
+			disconnectButton.IsEnabled = false;
+			commandButton.IsEnabled = false;
+		}
+
+		private void closeConnection()
+		{
+			sshclient.Disconnect();
+			sshclient.Dispose();
+			sshclient = null;
+		}
+
+		private void runCommand()
+		{
+
+			var cmd = sshclient.CreateCommand(command.Text);
+
+			output.Text += $"${command.Text}\n";
+			var result = cmd.Execute();
+			output.Text += $"{cmd.Result}\n";
+
+			var reader = new StreamReader(cmd.ExtendedOutputStream);
+
+			output.Text += reader.ReadToEnd();
 
 
+		}
 
-        }
-        
-        public ConnectionInfo Connect()
-        {
-             address = host.Text;
-             user = username.Text;
-             pass = password.Password;
+		private void commandButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (command.Text != "")
+			{
+				runCommand();
 
-            ConnectionInfo connection = new ConnectionInfo(address, user,
-                new AuthenticationMethod[]
-                {
-                    new PasswordAuthenticationMethod(user,pass)
-                });
+			}
+			else
+			{
+				MessageBox.Show("Command input is empty.");
+			}
+		}
 
-            return connection;
-        }
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				File.WriteAllText("output.log", output.Text);
+				MessageBox.Show("Log saved.");
 
-        private void disconnectButton_Click(object sender, RoutedEventArgs e)
-        {
-            //sshclient.Disconnect();
-            //sshclient.Dispose();
-            closeConnection();
-            output.Text += "\nDisconnected from server.";
-            connectButton.IsEnabled = true;
-            disconnectButton.IsEnabled = false;
-        }
+			}
+			catch (Exception ex)
+			{
+				Console.Write(ex.Message);
+			}
 
-        private void closeConnection()
-        {
-            sshclient.Disconnect();
-            sshclient.Dispose();
-            sshclient = null;
-
-        }
-
-        private void runCommand()
-        {
- 
-                var cmd = sshclient.CreateCommand(command.Text);
-
-                output.Text += $"${command.Text}\n";
-                var result = cmd.Execute();
-                output.Text += $"{cmd.Result}\n";
-
-                var reader = new StreamReader(cmd.ExtendedOutputStream);
-
-                output.Text += reader.ReadToEnd();
-
-          
-        }
-
-        private void commandButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (command.Text != "")
-            {
-                runCommand();
-      
-            }
-            else
-            {
-                MessageBox.Show("Command input is empty.");
-            }
-        }
-    }
+		}
+	}
 }
